e515312b198c70e4c78eb97365553a5603c58829
arburhan <buthan190629@outlook.com>
2022-08-20 23:45:31 +0600

    add font awsome icon

--- src/Pages/Services/Service.js
+++ src/Pages/Services/Service.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Service = ({ service }) => {
-    const { _id, name, image } = service;
+    const { _id, name, image, instructor, duration, price } = service;
     const navigate = useNavigate();
     const handleOrder = (id) => {
         navigate(`/purchase/${id}`);
@@ -15,7 +15,15 @@ const Service = ({ service }) => {
                 </figure>
                 <div class="card-body items-center text-center">
                     <h2 class="card-title">{name}</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <p>By <span className='text-secondary' >{instructor}  </span> </p>
+                    <div className='flex gap-12 py-2' >
+                        <div>
+                            <i class="fas fa-clock text-secondary"></i> {duration} hr
+                        </div>
+                        <div className='text-[red]' >
+                            <i class="fa-solid fa-dollar-sign"></i> {price}
+                        </div>
+                    </div>
                     <div class="card-actions">
                         <button onClick={() => { handleOrder(_id) }} class="btn btn-accent">Enroll</button>
                     </div>
