8b9fea8b6c38be22bc1823b2c41596197ac3ccf2
arburhan <buthan190629@outlook.com>
2022-08-21 00:09:17 +0600

    edit navbar

--- src/Pages/Shared/NavBar.js
+++ src/Pages/Shared/NavBar.js
@@ -25,12 +25,21 @@ const NavBar = () => {
         <li>
             <Link to=''>Contact Us</Link>
         </li>
-        <li>
+        {
+            user ? <li className='items-center' > {user.displayName} <button onClick={handleSignOut} >Logout <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+            </svg></button> </li> :
+                <li>
+                    <Link to='/logIn'>Login</Link>
+                </li>
+        }
+
+        {/*  <li>
             {user ?
                 <>
                     <Link to='' class="justify-between">
                         <figure style={{ width: 30 }} >
-                            {/* <img src={user?.photoURL === null ? userImage : user?.photoURL} alt="" /> */}
+
                             <img src={userImage} alt="" />
                         </figure>
                     </Link>
@@ -44,7 +53,8 @@ const NavBar = () => {
                     </ul>
                 </> : <Link to='/logIn'>Login</Link>
             }
-        </li>
+        </li> */}
+
     </>
     return (
         <nav>
